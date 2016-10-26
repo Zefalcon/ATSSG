9d6d5185564dc9e32c2284246d734f8e8fab54bc
Julie Kaplan <jak266@case.edu>
2016-10-25 20:48:55 -0400

    Added Attack Meta Action
    Wrote Meta Action that allows AI to attack units

---
+++ AI/AttackMeta.java
@@ -0,0 +1,23 @@
+package ATSSG.AI;
+
+import ATSSG.Action;
+import ATSSG.AttackAction;
+import ATSSG.Unit;
+
+public class AttackMeta extends MetaAction {
+	Unit attacker;
+	Unit target;
+
+	public AttackMeta(Unit attacker, Unit target) {
+		this.attacker = attacker;
+		this.target = target;
+	}
+	
+	@Override
+	public Action nextAction() {
+		if (attacker.validTargets().contains(target)) return new AttackAction(1, attacker, target);
+		//TODO: reuse previous moves on unmoving target
+		return new MoveMeta(target.getContainingCell(), attacker).nextAction();
+	}
+
+}
