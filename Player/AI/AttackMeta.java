a6864616eb94741a6440c082742cb81658282a61
Julie Kaplan <jak266@case.edu>
2016-10-27 17:12:32 -0400

    Fixed AttackMeta
    Now uses new Entity Code to check if it can attack.

--- Player/AI/AttackMeta.java
+++ Player/AI/AttackMeta.java
@@ -24,7 +24,7 @@ public class AttackMeta extends MetaAction {
 	
 	@Override
 	public Action nextAction() {
-		if (attacker.validTargets().contains(target)) return new AttackAction(1, attacker, target);
+		if (attacker.canAttack(target)) return new AttackAction(1, attacker, target);
 		//TODO: reuse previous moves on unmoving target
 		return new MoveMeta(target.getContainingCell(), attacker).nextAction();
 	}
