80a1892ea6e29328bc9cc37bace9467aafe2546e
Turtlelord26 <jltalbott47@gmail.com>
2016-11-13 11:16:47 -0500

    Minimap Clarity
    Should be more obvious where UI component boundaries are now.

--- Minimap.java
+++ Minimap.java
@@ -18,7 +18,7 @@ public class Minimap extends UIContainer<Cell> {
 	public Minimap(Collection<Cell> content, int width, int height, Player owner) {
 		super(content, width, height, owner);
 		view = new JPanel();
-		view.add(new JLabel(new ImageIcon(Paths.get("src/ATSSG/Art/BlankLabel.png").toString())));
+		view.add(new JLabel(new ImageIcon(Paths.get("src/ATSSG/Art/BigBlankLabel.png").toString())));
 		//view.setBounds(xLoc, yLoc, width, height);
 		view.setPreferredSize(getSize());
 		view.setVisible(true);
