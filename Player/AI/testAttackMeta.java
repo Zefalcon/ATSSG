d070d51b7d568ca28cbfb7a76e68259cf7e1466f
Julie Kaplan <jak266@case.edu>
2016-11-18 10:46:59 -0500

    Fixed testAttackMeta
    Added extra necessary Fields

--- Player/AI/testAttackMeta.java
+++ Player/AI/testAttackMeta.java
@@ -2,19 +2,29 @@ package ATSSG.Player.AI;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+
 import org.junit.Test;
 
 import ATSSG.Cell;
+import ATSSG.RCommodityType;
 import ATSSG.UnitType;
+import ATSSG.Entities.Entity;
 import ATSSG.Entities.Unit;
 import ATSSG.Player.HumanPlayer;
+import ATSSG.Player.Player;
 
 public class testAttackMeta {
 
+	public HumanPlayer setUpHuman() {
+		return new HumanPlayer(new Hashtable<RCommodityType, Integer>(), new ArrayList<Entity>(), null);
+	}
+
 	@Test
 	public void testIsDone() {
-		Unit attacker = new Unit(UnitType.Soldier, new HumanPlayer(null, null, null), new Cell(null, null, null, 0, 0));
-		Unit target = new Unit(UnitType.Soldier, new HumanPlayer(null, null, null), new Cell(null, null, null, 0, 0));
+		Unit attacker = new Unit(UnitType.Soldier, setUpHuman(), new Cell(null, null, null, 0, 0));
+		Unit target = new Unit(UnitType.Soldier, setUpHuman(), new Cell(null, null, null, 0, 0));
 		MetaAction m = new AttackMeta(attacker, target);
 		assert(!m.isDone());
 		target.doDamage(UnitType.Soldier.maxHP);
