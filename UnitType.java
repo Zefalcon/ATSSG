36ebf2e4c33cbc2be5e4c5857e802de3b6909593
TomShkurti <TomShkurti.school@gmail.com>
2016-11-30 17:59:06 -0500

    New units are in, pictures will be added as they are created.

--- UnitType.java
+++ UnitType.java
@@ -12,12 +12,86 @@ import javax.imageio.ImageIO;
 public enum UnitType {
 	
 	Void("Void", "", Paths.get("src/ATSSG/Art/BlankLabel.png").toString(), 0, null, 0, null, 0, 0),
-	Soldier("Solder", "Soldier.", Paths.get("src/ATSSG/Art/DemoUnit.png").toString(), 5,
-			new InlineMap<TerrainType,Double>().returnPut(TerrainType.GRASS, 1.0), 1, AttackType.STABBING, 1, 500);//flag instakill shortcut code.
-	//RealSoldier("Soldier", "A stronger melee unit.", null, 5, new InlineMap<TerrainType,Double>().returnPut(TerrainType.GRASS, 1.0), 1, AttackType.STABBING, 1, 3),
+	
+	//Soldier("Solder", "Soldier.", Paths.get("src/ATSSG/Art/DemoUnit.png").toString(), 5,
+	//		new InlineMap<TerrainType,Double>().returnPut(TerrainType.GRASS, 1.0), 1, AttackType.STABBING, 1, 500);//flag instakill shortcut code.
+	Soldier(
+		"Soldier",
+		"A standard melee unit.",
+		Paths.get("src/ATSSG/Art/DemoUnit.png").toString(),
+		10,
+		new InlineMap<TerrainType,Double>()
+			.returnPut(TerrainType.GRASS, 1.0)
+			.returnPut(TerrainType.ROUGH_TERRAIN, 2.0),
+		3,
+		AttackType.STABBING,
+		1,
+		4
+	),
+	Archer(
+		"Archer",
+		"A basic ranged unit.",
+		null,
+		10,
+		new InlineMap<TerrainType,Double>()
+			.returnPut(TerrainType.GRASS, 1.0)
+			.returnPut(TerrainType.ROUGH_TERRAIN,  2.0),
+		3,
+		AttackType.STABBING,
+		3,
+		2
+	),
+	Guardtower(
+		"Guardtower",
+		"A stationary, ranged defensive structure.",
+		null,
+		15,
+		new InlineMap<TerrainType,Double>(),
+		0,
+		AttackType.STABBING,
+		5,
+		2
+	),
+	SeigeEngine(
+		"Seige Engine",
+		"A large, moving weapon.",
+		null,
+		5,
+		new InlineMap<TerrainType,Double>()
+			.returnPut(TerrainType.GRASS, 1.0),
+		1,
+		AttackType.STABBING,
+		8,
+		20
+	),
+	Mountaineer(
+		"Mountaineer",
+		"A unit trained in navigating difficult terrain.",
+		null,
+		8,
+		new InlineMap<TerrainType,Double>()
+			.returnPut(TerrainType.GRASS, 1.0)
+			.returnPut(TerrainType.ROUGH_TERRAIN, 1.0)
+			.returnPut(TerrainType.MOUNTAIN, 1.5),
+		3,
+		AttackType.STABBING,
+		1,
+		3
+	),
+	Cavalry(
+		"Cavalier",
+		"A fast, hard-hitting, fragile unit.",
+		null,
+		4,
+		new InlineMap<TerrainType, Double>().returnPut(TerrainType.GRASS, 1.0),
+		8,
+		AttackType.STABBING,
+		1,
+		6
+	);
 	//Archer("Archer", "A ranged unit.", null, 5, new InlineMap<TerrainType,Double>().returnPut(TerrainType.GRASS, 1.0), 1, AttackType.SHOOTING, 3, 1);
 
-	HashMap<TerrainType,Double> warriorPassage = new HashMap<>();
+	HashMap<TerrainType,Double> warriorPassage = new HashMap<>();//flag what does this do?
 	
 	public String name;
 	public String description;
