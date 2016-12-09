a1f236ee324b2bd9f6268477d7aa26ecb251cad5
TomShkurti <TomShkurti.school@gmail.com>
2016-12-09 13:31:45 -0500

    Loadsave unit test is working.

--- TestGameMap.java
+++ TestGameMap.java
@@ -3,11 +3,15 @@ package ATSSG;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import ATSSG.Entities.Unit;
 import ATSSG.Enums.TerrainType;
 
 public class TestGameMap {
@@ -41,6 +45,36 @@ public class TestGameMap {
 	public void checkTerrain(){
 		assertEquals(testMap.getCell(5, 5).getTerrainType(), TerrainType.GRASS);
 		assertEquals(testMap.getCell(3, 4).getTerrainType(), TerrainType.GRASS);
+	}
+	
+	@Test
+	public void checkLoadsave(){
+		try{
+			GameMap savemap = new GameMap(new File("src/ATSSG/Maps/Large.map"));
+			FileOutputStream out = new FileOutputStream("src/ATSSG/testsave.sav");
+			out.write(savemap.Save());
+			out.close();
+
+			File file = new File("src/ATSSG/testsave.sav");
+			FileInputStream in = new FileInputStream(file);
+			byte[] contents = new byte[(int)file.length()];
+			in.read(contents);
+			in.close();
+			
+			GameMap loadmap = new GameMap();
+			loadmap.update(GameMap.Load(contents));
+			
+			assertEquals(savemap.getEntities().size(), loadmap.getEntities().size());
+			for(int i = 0; i < loadmap.getEntities().size(); i++){
+				assertEquals(savemap.getEntities().get(i).toString(), loadmap.getEntities().get(i).toString());
+				assertEquals(((Unit)savemap.getEntities().get(i)).getHitPoints(), ((Unit)savemap.getEntities().get(i)).getHitPoints());
+			}
+		}catch(FileNotFoundException x){
+			//Silently drop.
+		}
+		catch(Exception x){
+			System.out.println(x.toString());
+		}
 	}
 	
 	/*@Test
